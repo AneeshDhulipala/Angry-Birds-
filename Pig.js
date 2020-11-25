ac3a7ce0795cc16ceef62361366a566c443d759a
AneeshDhulipala <72055587+AneeshDhulipala@users.noreply.github.com>
2020-11-25 10:42:37 -0600

    Add files via upload
    Made 2 constraint bodies. One connected  to the bird.

--- Pig.js
+++ Pig.js
@@ -1,10 +1,7 @@
-class Pig extends ParentClass {
-    constructor(x, y, width, height) {
-    super(x,y,width,height)
-      this.image = loadImage("images/enemy.png")
-    }
-    display(){
-    super.display()
-    }
-  };
-  
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+  }
+
+};
